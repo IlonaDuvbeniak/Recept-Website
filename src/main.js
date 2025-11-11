0473a68456422bab198c77872f11c0b7b5d31dda
siriprydz <siriprydz@gmail.com>
2025-11-11 10:50:41 +0100

    add vite and vue routes

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Nothing is changed from the default file here
+
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
