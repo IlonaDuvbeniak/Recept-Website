a1accedb9056f71676cdede9e558730d9d60b437
Ilona Duvbeniak <167776440+IlonaDuvbeniak@users.noreply.github.com>
2025-11-11 11:42:02 +0100

    add card components (#53)
    * add card components
    
    * everything's good

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+    // import { RouterLink, RouterView } from 'vue-router'
+    import Card from './components/Card.vue'
+
+    export default {
+        components: {
+            Card
+        },
+        data() {
+            return {  
+                coctails: []
+            }
+        },
+        async mounted() {
+            try {
+                const res = await fetch('./recipesData.json')
+                const data = await res.json()
+                this.coctails = data.coctails
+            } catch (err) {
+                console.error(err)
+            }
+        }
+    }
 </script>
 
 <template>
-
+    <div class="cards-container">
+        <Card
+            v-for="coctail in coctails"
+            :key="coctail.id"
+            :categori="coctail.categori"
+            :name="coctail.name"
+            :rating="coctail.rating"
+            :ingridients="coctail.ingridients"
+            :time="coctail.time"
+            :image="coctail.image"
+            :label="coctail.name"
+        />
+    </div>
 </template>
 
 <style scoped>
-
+    .cards-container {
+        display: grid;
+        grid-template-columns: auto auto auto;
+        width: 100%;
+        gap: 24px;
+    }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,112 @@
+<script>
+    import ButtonCard from './ButtonCard.vue'
+    import InfoBlockCard from './InfoBlockCard.vue';
+
+    export default {
+        components: {
+            ButtonCard,
+            InfoBlockCard
+        },
+        props: {
+            categori: String,
+            name: String,
+            rating: Number,
+            ingridients: Number,
+            time: Number,
+            image: String,
+            label: String
+        }
+    }
+</script>
+
+<template>
+    <div class="card">
+        <div class="image-container">
+            <img :src="image" :alt="label">
+        </div>
+
+        <div class="name-container">
+            <div class="text-name-container">
+                <a href="#">{{ categori }}</a>
+                <h2>{{ name }}</h2>
+            </div>
+            <ButtonCard><img src="/img/arrow-btn.svg"/></ButtonCard>
+        </div>
+
+        <div class="info-recipe-card">
+            <InfoBlockCard icon="/img/star-rating.svg" :value="rating" label="Rating"/>
+            <InfoBlockCard icon="/img/grocery.svg" :value="ingridients" label="Ingridients"/>
+            <InfoBlockCard icon="/img/timer.svg" :value="time" label="Minutes"/>
+        </div>
+        
+    </div>
+</template>
+
+
+<style scoped>
+    .card {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        border-style: solid;
+        border-color: #D80A3D;
+        border-width: 1px 3px 3px 1px;
+    }
+
+    .image-container {
+        display: flex;
+        width: 100%;
+        padding: 16px;
+    }
+
+    .image-container img {
+        width: 100%;
+        /* min-height: 269px; */
+    }
+    
+
+    .name-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: 100%;
+        border-top: 1px solid #D80A3D;
+        border-bottom: 1px solid #D80A3D;
+    }
+
+    .text-name-container {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        height: 100%;
+        width: 66.666%;
+        padding: 16px 0px 16px 16px;
+    }
+
+
+    a {
+        color: #D80A3D;
+        font-family: "Instrument Sans";
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 16px; 
+        text-decoration: none;
+    }
+
+    h2 {
+        color: #D80A3D;
+        font-family: "Instrument Sans";
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 24px; 
+    }
+
+    .info-recipe-card {
+        display: flex;
+        align-items: center; 
+    }
+
+
+</style>
