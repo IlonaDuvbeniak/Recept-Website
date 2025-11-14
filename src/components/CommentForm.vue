9605178162113c2724ad9f3e410662234f2a9e01
NatB-dev <natassjaberkan@gmail.com>
2025-11-14 17:28:40 +0100

    Adds counting function and halfway on creating object function

--- src/components/CommentForm.vue
+++ src/components/CommentForm.vue
@@ -1,38 +1,57 @@
 <script>
 export default {
-  name: 'CommentForm',
-  data() {
-    return {
-        name: '',
-        title: '',
-        writtenComment: '',
-        clickedButton: console.log("hej!"),
-        hej: console.log("hej från input!"),
-        showText: "",
-        buttonClicked: false,
-        time: '',
-        
-    }
-},
-  methods: {
-     submitAll () {
-      this.submitComment();
-      this.commentTime();
+    name: 'CommentForm',
+    data() {
+        return {
+            name: '',
+            title: '',
+            writtenComment: '',
+            clickedButton: console.log("hej!"),
+            hej: console.log("hej från input!"),
+            showText: "",
+            buttonClicked: false,
+            time: '',
+            id: 0
+            
+        }
     },
-    submitComment() {
-      // Logic to submit the comment
-      console.log(this.name + " " + this.title + " " + this.writtenComment);
-    },
-    commentTime() {
-      const dateOfComment = this.time = Date();
-      const splittedDate = dateOfComment.split(" ");
-      console.log(splittedDate[0], splittedDate[2], splittedDate[1], splittedDate[4].slice(0,5));
-     
-      
-      
-      
+    methods: {
+        submitAll () {
+        this.submitComment();
+        this.commentTime();
+        },
+        submitComment() {
+        // Logic to submit the comment
+        console.log(this.name + " " + this.title + " " + this.writtenComment);
+        },
+
+        // eventuellt göra return på time så det kan matas in i createCommentObj
+        commentTime() {
+        const dateOfComment = this.time = Date();
+        const splittedDate = dateOfComment.split(" ");
+        console.log(splittedDate[0], splittedDate[2], splittedDate[1], splittedDate[4].slice(0,5));
+        },
+
+        // jobba på att få in id, namn, title, writtenComment och time här:
+        createCommentObj(id, name, title, writtenComment, time) {
+            const commentObj = new Object({
+                id: this.id,
+                name: this.id,
+                title: this.id,
+                writtenComment: this.writtenComment,
+                time: this.time 
+            });
+            console.log(commentObj);
+            // return commentObj;
+        },
+
+        // Använd det här för att skapa nytt id nummer för varje kommentar.
+        // Tex från att hämtat data från api med fetch. 
+        createIdNumber () {
+            this.id++;
+            console.log (this.id);
+        }
     }
-  }
 }
 
 </script>
@@ -52,7 +71,10 @@ export default {
   <!-- <button v-on:click="clickedButton">Konsollen</button>
   <input id="input1" v-model="showText" v-on:input="hej" placeholder="skriv..."></input> -->
 
+  <button v-on:click="createCommentObj">Skapa Objekt</button>
 
+  <button v-on:click="createIdNumber">Räkna - funktion att använda för varje nytt Id</button>
+  <div>{{ id }}</div>
 
   <p>Det här är från comment form</p>
 
