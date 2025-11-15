57cea55c45b4fc0a757338d14b4ddb72567680fb
siriprydz <siriprydz@gmail.com>
2025-11-15 16:02:45 +0100

    Add breadcrumbs component and dynamic route functionality (#76)

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,5 +1,24 @@
+<script>
+import data from '/recipesData.json';
+import BreadCrumbs from '@/components/BreadCrumbs.vue'
+
+export default {
+    name: 'recipe',
+    components: { BreadCrumbs },
+    data() {
+        return {
+            recipe: null
+        }
+    },
+    created() {
+    const id = Number(this.$route.params.id)
+    this.recipe = data.coctails.find(drink => drink.id === id)
+  },
+
+}
+</script>
+
 <template>
-    <h1>Recipe</h1>
 </template>
 
 <style>
