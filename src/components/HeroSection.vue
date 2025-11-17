301b99cacf0cfc0c82d21df03b35b3e35c573658
siriprydz <siriprydz@gmail.com>
2025-11-17 20:48:28 +0100

    Add video to HeroSection

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,6 +3,14 @@ export default {
   name: 'HeroSection',}
 </script>
 <template>
+    <video id="hero-movie">
+        <source src="/videos/HeroMovie.mp4" type="video/mp4" />
+    </video>
 </template>
 <style scoped>
+#hero-movie {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+}
 </style>
