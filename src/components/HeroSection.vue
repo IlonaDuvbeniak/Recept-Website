0df57059257d73247c674a99ca44c816db5c52aa
siriprydz <siriprydz@gmail.com>
2025-11-19 14:11:27 +0100

    add autoplay

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -3,22 +3,22 @@ import Header from './Header.vue';
 import Button from './Button.vue';
 
 export default {
-  name: 'HeroSection',
-  components: {
-    Header,
-    Button
-  },
+    name: 'HeroSection',
+    components: {
+        Header,
+        Button
+    },
 }
 </script>
 <template>
     <div class="hero-section">
-        <Header class="header-component"/>
+        <Header class="header-component" />
 
-        <video class="hero-movie" muted loop>
-            <source src="/videos/HeroMovie.mp4" type="video/mp4" aria-label="Girl sipping on spooky drink"/>
+        <video class="hero-movie" autoplay muted loop>
+            <source src="/videos/HeroMovie.mp4" type="video/mp4" aria-label="Girl sipping on spooky drink" />
         </video>
 
-         <div class="title-and-button">
+        <div class="title-and-button">
             <h1>CHEERS <br></br>WITCHES!</h1>
             <Button btnText="Boo season!" variant="primary" :showArrow="true" :disabled="false"></Button>
         </div>
@@ -30,12 +30,14 @@ export default {
 .hero-section {
     margin-bottom: 0px;
 }
+
 .hero-movie {
     position: absolute;
     top: 0;
     width: 100%;
     height: auto;
 }
+
 .header-component {
     position: absolute;
     top: 0;
@@ -43,6 +45,7 @@ export default {
     width: 100%;
     z-index: 1;
 }
+
 .swirl-text {
     position: relative;
     margin-top: 0px;
@@ -50,17 +53,20 @@ export default {
     transform: translateX(-50%);
     width: 100%;
 }
+
 h1 {
     color: var(--red-color);
     font-family: "Kalnia";
     font-size: 88px;
     line-height: 1
 }
+
 .title-and-button {
     position: relative;
     left: 60%;
     margin-top: 200px;
 }
+
 Button {
     margin-top: 42px;
 }
