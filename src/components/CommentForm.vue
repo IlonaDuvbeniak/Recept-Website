754114e384d4263d769d8888fdf7b4be423b3589
NatB-dev <natassjaberkan@gmail.com>
2025-11-20 15:12:00 +0100

    Comment section (#86)
    * Adds submit all function with date and comment to Comment Section.
    
    * Adds counting function and halfway on creating object function
    
    * Adds a placeholder json file for registered comments
    
    * Add a fetch from the placefolderCommentsAPI
    
    * adds functional comment-form

---
+++ src/components/CommentForm.vue
@@ -0,0 +1,236 @@
+<script>
+
+export default {
+
+    name: 'CommentForm',
+    data() {
+        return {
+            newName: '',
+            newTitle: '',
+            newWrittenComment: '',
+            newTime: '',  
+            id: 0,
+            submittedId: "",
+            submittedName: "",
+            submittedTitle: "",
+            submittedWrittenComment: "",
+            submittedTime: "",
+            commentsArray: [    
+                { id: 1, name: "Anna", title: "Hej!", writtenComment: "Detta är en testkommentar", time: "17 Nov 2025" },
+                { id: 2, name: "Moa", title: "Glassigt", writtenComment: "Rolig att ha till festen. De andra blev väldigt nyfikna så nästa gång ska jag nog bjuda på den till alla", time: "18 Nov 2025" },
+                { id: 3, name: "Alva", title: "Gott Recept", writtenComment: "Jag älskar detta recept, tack för att du delade!", time: "19 Nov 2025" },
+             ],
+        }
+
+    },
+    methods: {
+        submitAll () {
+            this.commentTime();
+
+            this.commentsArray.push({id: this.commentsArray.length + 1, name: this.newName, title: this.newTitle, writtenComment: this.newWrittenComment, time: this.newTime});
+
+            this.newName = "";
+            this.newTitle = "";
+            this.newWrittenComment = "";
+            this.newTime = "";
+
+        },
+                
+        commentTime() {
+        const dateOfComment = Date();
+        const splittedDate = dateOfComment.split(" ");
+        console.log(splittedDate[0], splittedDate[2], splittedDate[1], splittedDate[4].slice(0,5));
+        
+        this.newTime = `${splittedDate[0]} ${splittedDate[2]} ${splittedDate[1]} ${splittedDate[4].slice(0,5)}`;        
+        return this.newTime;
+        },
+    }
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="submitAll">
+    <label>
+        <div class="comment-form">
+        <div class="comment-form-top">
+            <h2 class="recept-name">Kommentar</h2>
+            <input v-model="newName" placeholder="Ditt namn"></input>
+        </div>
+        
+        <input v-model="newTitle" placeholder="Rubrik max 12 tecken"></input>
+        <textarea v-model="newWrittenComment" placeholder="Skriv din kommentar"></textarea>
+        <button class="btn-comment-form" type="submit">Skicka -></button>
+        </div>
+    </label>
+  </form>
+   
+
+  <!-- <button v-on:click="commentTime">tid</button> -->
+
+  
+
+<div class="comment-cards-container">
+    <div v-for="comment in commentsArray" :key="comment.id" class="comment-card">
+        
+        <div class="comment-cards-top">
+            <p class="cocktail-name"><strong>{{ comment.name }}</strong></p>
+            <p class="p-time">{{ comment.time }}</p>
+        </div>
+        <h3 class="title-comment-cards">{{ comment.title }}</h3>
+        <p class="main-text">{{ comment.writtenComment }}</p>
+        
+    </div> 
+</div> 
+
+</template>
+
+<style scoped>
+
+
+
+button {
+    background-color: var(--red-color);
+    color: #ffffff;
+    border-radius: 50px;
+    border-color: var(--red-color);
+    box-shadow: none;
+    padding: 8px 35px 8px 35px;
+
+
+    font-family: instrument-sans, sans-serif;
+    font-style: normal;
+    font-size: 20px;
+    font-weight: 600px;
+    width: 200px;
+}
+
+.btn-comment-form {
+    align-self: center;
+    margin-bottom: 20px;
+}
+.btn-comment-form:hover {
+    background-color: var(--white-color);
+    border: 3px solid var(--red-color);
+    color: var(--red-color)
+}
+
+.recept-name {
+    color: var(--red-color);
+    font-family: Kalnia;
+    font-size: 56px;
+    font-weight: 700;
+    line-height: 64px; 
+
+    transition: transform 1s ease, opacity 1s ease;
+    overflow: hidden;
+}
+
+.title-comment-cards {
+    color: var(--red-color);
+    font-family: Kalnia;
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 40px; 
+    padding-bottom: 7px;
+
+    transition: transform 1s ease, opacity 1s ease;
+    padding-bottom: 30px;
+   
+}
+
+p {
+    color: var(--red-color);
+        font-family: "Instrument Sans";
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 24px; 
+        text-decoration: none;
+
+}
+
+.cocktail-name {
+    font-size: 24px;
+    line-height: 24px;
+    font-weight: 600px;;
+}
+
+.main-text {
+    font-size: 16px;
+
+}
+
+.p-time {
+    font-size: 14;
+}
+
+input, textarea {
+    
+    font-family: "Instrument Sans";
+    font-size: 20px;
+    color: var(--red-color);
+    -webkit-text-fill-color: var(--red-color);
+
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; 
+    padding: 8px 35px 8px 35px;
+    height: 50px;
+    border-radius: 100px;
+    border: 2px solid var(--red-color);
+    background-color: var(--white-color);
+    resize: none;
+    }
+
+    input:hover, textarea:hover {
+    border-color: var(--dark-red-color);
+    color: var(--dark-red-color);
+    -webkit-text-fill-color: var(--dark-red-color);
+}
+   
+
+.comment-card {
+    background-color: var(--baby-pink-color);
+    padding: 22px;
+    width: 32%;
+    padding-bottom: 60px;
+    
+}
+
+.comment-cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: space-between;
+    margin: 0 auto;
+    gap: 20px;
+    width: 97%;
+    
+}
+
+.comment-cards-top {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 35px;
+    padding-top: 25px;
+}
+
+.comment-form-top {
+    display: flex;
+    justify-content: space-between;
+    gap: 24px;
+}
+
+.comment-form {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    width: 792px;
+    margin: 0 auto;
+    margin-bottom: 126px;
+   
+}
+
+</style>
--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,16 +1,20 @@
 <script>
+
   import { getData } from '../FetchData.vue'
   import ReceptCard from '@/components/RecipeComponents/ReceptCard.vue';
   import HowToDo from '@/components/RecipeComponents/HowToDo.vue';
   import RatingCard from '@/components/RecipeComponents/RatingCard.vue';
-
+  import CommentForm from '../components/CommentForm.vue';
+  
+  
   export default {
       name: 'recipe',
 
       components: {
         ReceptCard,
         HowToDo,
-        RatingCard
+        RatingCard,
+        CommentForm
       },
 
       props: ['slug'],
@@ -32,13 +36,12 @@
           console.error(err)
         }
       }
-
   }
-
 
 </script>
 
 <template>
+
     <svg xmlns="http://www.w3.org/2000/svg" width="1440" height="129" viewBox="0 0 1440 129" fill="none" class="element-header">
       <path d="M-55.5 -45.5L34.3619 23.9578C168.915 127.959 359.645 120.918 487.534 8.82223C619.611 -106.946 818.329 -108.632 950.909 6.56029C1081.18 119.743 1274.36 121.544 1406.71 10.81L1529 -91.5" stroke="#A5D1D0" stroke-width="62"/>
     </svg>
@@ -61,7 +64,13 @@
     <div v-else>
       <h2>Receptet hittades inte</h2>
     </div>
+  
+    <section>
+        <CommentForm />
+    </section>
+  
 </template>
+  
 
 <style scoped>
   .element-header{
@@ -71,6 +80,5 @@
     left: 0;
     z-index: -1;
   }
-  
 
 </style>
