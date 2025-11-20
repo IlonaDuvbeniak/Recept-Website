1778f68ecd85c6cfc87416e9818bf7dfaf6fd4e1
siriprydz <siriprydz@gmail.com>
2025-11-20 18:52:52 +0100

    Add dynamic router-links to CategoryView from cards

--- src/components/CardsGrid.vue
+++ src/components/CardsGrid.vue
@@ -1,52 +1,44 @@
 <script>
-    import { getData} from '../FetchData.vue'
-    import Card from '../components/Card.vue'
+import { getData } from '../FetchData.vue'
+import Card from '../components/Card.vue'
 
-    export default {
-        components: {
-            Card
-        },
-        data() {
-            return {
-                coctails: []
-            }
-        },
-        async mounted() {
-            try {
-                const data = await getData()
-                this.coctails = data.coctails
-            } catch (err) {
-                console.log(err)
-            }
+export default {
+    components: {
+        Card
+    },
+    data() {
+        return {
+            coctails: []
+        }
+    },
+    async mounted() {
+        try {
+            const data = await getData()
+            this.coctails = data.coctails
+        } catch (err) {
+            console.log(err)
         }
     }
+}
 </script>
 
 <template>
     <div class="page-container">
-    <div class="cards-container">
-        <Card
-            v-for="coctail in coctails"
-            :key="coctail.id"
-            :categori="coctail.categori"
-            :name="coctail.name"
-            :rating="coctail.rating"
-            :ingridients="coctail.ingridients"
-            :time="coctail.time"
-            :image="coctail.image"
-            :label="coctail.name"
-            :to="{ name: 'recipe', params: { slug: coctail.slug } }"
-        />
-    </div>
+        <div class="cards-container">
+            <Card v-for="coctail in coctails" :key="coctail.id" :categori="coctail.categori"
+                :categorySlug="coctail.categorySlug" :name="coctail.name" :rating="coctail.rating"
+                :ingridients="coctail.ingridients" :time="coctail.time" :image="coctail.image" :label="coctail.name"
+                :to="{ name: 'recipe', params: { slug: coctail.slug } }" />
+        </div>
     </div>
 </template>
 
 <style scoped>
-    .cards-container {
-        display: grid;
-        grid-template-columns: auto auto auto;
-        width: 100%;
-        gap: 24px;
-        padding: 0 120px;
-    }
+.cards-container {
+    display: grid;
+    grid-template-columns: auto auto auto;
+    width: 100%;
+    gap: 24px;
+    padding: 0 120px;
+}
 </style>
