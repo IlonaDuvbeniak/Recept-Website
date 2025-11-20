e419628e30fca436dd012ad0030dd97f1f898b8d
siriprydz <siriprydz@gmail.com>
2025-11-20 16:14:47 +0100

    Add CategoryView component and router-path

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,20 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import RecepiesView from "../views/RecipesView.vue";
 import RecipeView from "../views/RecipeView.vue";
+import CategoryView from "@/views/CategoryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: HomeView },
     { path: "/recipes", name: "recepies", component: RecepiesView },
-    { path: "/recipes/:slug", name: "recipe", component: RecipeView, props: true },
+    { path: "/recipes/:category", name: "category", component: CategoryView },
+    {
+      path: "/recipes/:slug",
+      name: "recipe",
+      component: RecipeView,
+      props: true,
+    },
   ],
 });
 
