49a197795f89f6ed79aec568006e8c9a84619334
siriprydz <siriprydz@gmail.com>
2025-11-21 17:17:43 +0100

    Add SearchBar component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: "SearchBar"
+}
+</script>
+
+<template>
+    <div>
+        <label for="site-search">Sök efter recept</label>
+        <input type="search" class="site-search"></input>
+    </div>
+</template>
+
+<style scoped></style>
