1dc71d5032920d95803fc8d116570c4c2305ad8f
siriprydz <siriprydz@gmail.com>
2025-11-21 17:25:46 +0100

    add search icon

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,7 +7,9 @@ export default {
 <template>
     <div>
         <label for="site-search">Sök efter recept</label>
-        <input type="search" class="site-search"></input>
+        <input type="search" class="site-search">
+        <img src="/img/search-icon.svg">
+        </input>
     </div>
 </template>
 
