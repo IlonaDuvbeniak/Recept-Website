d30d05a91d4271d3892c726891a88932e7303364
siriprydz <siriprydz@gmail.com>
2025-11-21 17:19:51 +0100

    Import SearchBar component to RecipeView

--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,18 +1,21 @@
 <script>
 import CardsGrid from '@/components/CardsGrid.vue';
 import Button from '@/components/Button.vue'
+import SearchBar from '@/components/SearchBar.vue';
 
 export default {
     name: 'RecipesView',
     components: {
         CardsGrid,
         Button,
+        SearchBar
     }
 }
 </script>
 
 <template>
     <div class="container">
+        <SearchBar></SearchBar>
         <CardsGrid />
         <Button btnText="Visa fler" variant="primary" :showArrow="true" :disabled="false" @event="showMore"></Button>
     </div>
