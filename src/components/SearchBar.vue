2d8a3c5d1b28a77049399650ad51ebd49156c904
siriprydz <siriprydz@gmail.com>
2025-11-24 15:03:54 +0100

    Add more CSS on SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,58 +5,75 @@ export default {
 </script>
 
 <template>
-    <div class="search-bar-container">
-        <div class="search-bar">
+    <div class="search">
+        <form class="search-bar-container">
             <img class="search-icon" src="/img/search-icon.svg">
             <label for="site-search"></label>
-            <input type="search" class="site-search" placeholder="Search by vibe, not ingredient">
+            <input type="search" id="site-search" placeholder="Search by vibe, not ingredient" name="q"
+                aria-label="Search through recipes" required minlength="1" maxlength="30">
             </input>
-        </div>
+        </form>
     </div>
 </template>
 
 <style scoped>
-.search-bar-container {
+.search {
     width: 100%;
     padding: 0 120px;
-    display: flex;
-    flex-direction: column;
-
 }
 
-.search-bar {
+.search-bar-container {
+    padding: 0 0 0 65px;
+    display: flex;
+    flex-direction: row;
     border-top: 1px solid var(--red-color);
     border-left: 1px solid var(--red-color);
     border-right: 3px solid var(--red-color);
     border-bottom: 3px solid var(--red-color);
     border-radius: 50px;
-
 }
 
-.site-search {
+.search-bar-container:focus-within {
+    border-color: var(--dark-red-color);
+}
+
+#site-search {
+    padding-left: 10px;
+    ;
     border: none;
     color: var(--red-color);
     background-color: var(--white-color);
-    width: 90%;
     font-family: "Instrument Sans";
     font-size: 16px;
     font-weight: 400;
     line-height: 24px;
     letter-spacing: 1.28px;
-    margin: 10px;
+    outline: none;
+    field-sizing: content;
+    max-width: 100%;
 
+}
+
+input[type="search"]::-webkit-search-decoration,
+input[type="search"]::-webkit-search-cancel-button,
+input[type="search"]::-webkit-search-results-button,
+input[type="search"]::-webkit-search-results-decoration {
+    -webkit-appearance: none;
 }
 
 .search-icon {
     height: 30px;
-    padding-left: 20px;
+    padding-left: 0px;
     margin: 10px;
 }
 
 ::placeholder {
     color: var(--red-color);
     font-family: "Instrument Sans";
+    overflow: visible;
+}
 
-
+input:focus::placeholder {
+    color: transparent;
 }
 </style>
