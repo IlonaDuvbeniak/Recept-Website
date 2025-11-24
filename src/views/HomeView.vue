f26307e3fcbeb8dc80739f87638519d07c75e250
siriprydz <siriprydz@gmail.com>
2025-11-24 16:51:18 +0100

    Search bar (#109)
    * Add SearchBar component
    
    * Import SearchBar component to RecipeView
    
    * Import SearchBar component to HomeView
    
    * add search icon
    
    * Add CSS on SearchBar component
    
    * Add more CSS on SearchBar component
    
    * Add media query for SearchBar container
    
    * Add CSS change in SearchBar component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,29 +2,33 @@
 import HeroSection from '../components/HeroSection.vue';
 import CardsGrid from '@/components/CardsGrid.vue';
 import Button from '@/components/Button.vue'
+import SearchBar from '@/components/SearchBar.vue';
 export default {
   name: 'HomeView',
   components: {
     HeroSection,
     CardsGrid,
-    Button
+    Button,
+    SearchBar
   },
 }
 </script>
 <template>
-    <HeroSection/>
-    <div class="container">
-        <CardsGrid/>
-        <Button :to="{ name: 'recepies' }" btnText="Visa fler" variant="primary" :showArrow="true" :disabled="false"></Button>
-    </div>
+  <HeroSection />
+  <div class="container">
+    <SearchBar></SearchBar>
+    <CardsGrid />
+    <Button :to="{ name: 'recepies' }" btnText="Visa fler" variant="primary" :showArrow="true"
+      :disabled="false"></Button>
+  </div>
 </template>
 
 <style scoped>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 52px;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 52px;
+}
 </style>
--- src/views/RecipesView.vue
+++ src/views/RecipesView.vue
@@ -1,29 +1,32 @@
 <script>
-    import CardsGrid from '@/components/CardsGrid.vue';
-    import Button from '@/components/Button.vue'
+import CardsGrid from '@/components/CardsGrid.vue';
+import Button from '@/components/Button.vue'
+import SearchBar from '@/components/SearchBar.vue';
 
-    export default {
-        name: 'RecipesView',
-        components: {
-            CardsGrid,
-            Button
-        }
+export default {
+    name: 'RecipesView',
+    components: {
+        CardsGrid,
+        Button,
+        SearchBar
     }
+}
 </script>
 
 <template>
     <div class="container">
-        <CardsGrid/>
+        <SearchBar></SearchBar>
+        <CardsGrid />
         <Button btnText="Visa fler" variant="primary" :showArrow="true" :disabled="false" @event="showMore"></Button>
     </div>
 </template>
 
 <style scoped>
-    .container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 52px;
-    }
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 52px;
+}
 </style>
