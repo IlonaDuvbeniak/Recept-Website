94d1c3fe606b47de85e8cf90c050695b3e482b5f
Ilona Duvbeniak <ilonaduvbeniak@gmail.com>
2025-12-11 13:54:50 +0100

    comment every console-log

--- src/utils/fetchCategories.js
+++ src/utils/fetchCategories.js
@@ -3,11 +3,11 @@ let cachedCategories = null;
 export async function fetchCategories() {
   try {
     if (cachedCategories) {
-      console.log("Returning categories from cache");
+      // console.log("Returning categories from cache");
       return cachedCategories;
     }
 
-    console.log("Gets categories from API");
+    // console.log("Gets categories from API");
     const response = await fetch(
       "https://recipes.bocs.se/api/v1/d4e5f6a7-b8c9-4d0e-1f2a-3b4c5d6e7f8a/categories"
     );
@@ -16,7 +16,7 @@ export async function fetchCategories() {
     cachedCategories = data;
     return data;
   } catch (error) {
-    console.error("Error fetching categories:", error);
+    // console.error("Error fetching categories:", error);
     return [];
   }
 }
